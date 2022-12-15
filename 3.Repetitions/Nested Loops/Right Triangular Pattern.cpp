6adb9fc947d835450666f7eb9030b2341e4b6738
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-15 19:12:03 +0530

    Update Right Triangular Pattern.cpp

--- 3.Repetitions/Nested Loops/Right Triangular Pattern.cpp
+++ 3.Repetitions/Nested Loops/Right Triangular Pattern.cpp
@@ -5,8 +5,13 @@ int main()
 	int n;
 	cout<<"Enter n:";
 	cin>>n;
-	for(int i=0;i<n;i++)
+	for(int i=1;i<=n;i++)
 	{
-		
+		for(int j=1;j<=i;j++)
+		{
+			cout<<"* ";
+		}
+		cout<<"\n";
 	}
+	return 0;
 }
