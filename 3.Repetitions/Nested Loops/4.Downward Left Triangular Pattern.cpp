d4389231d3926574125d510660a0544b2769cb70
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-15 19:39:15 +0530

    Create 4.Downward Left Triangular Pattern.cpp

---
+++ 3.Repetitions/Nested Loops/4.Downward Left Triangular Pattern.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter n:";
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=i;j<=n;j++)
+		{
+			cout<<"* ";
+		}
+		cout<<"\n";
+		for(int k=1;k<=i;k++)
+		{
+			cout<<"  ";
+		}
+	}
+	return 0;
+}
