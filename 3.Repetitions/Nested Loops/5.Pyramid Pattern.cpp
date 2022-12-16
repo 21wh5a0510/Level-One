45eed711ac1fc607dc91ac6f629d0191cd877c13
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-16 21:03:08 +0530

    Update 5.Pyramid Pattern.cpp

--- 3.Repetitions/Nested Loops/5.Pyramid Pattern.cpp
+++ 3.Repetitions/Nested Loops/5.Pyramid Pattern.cpp
@@ -19,3 +19,28 @@ int main()
 	}
 	return 0;
 }
+
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter n:";
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=n;j>i;j--)
+		{
+			cout<<"  ";
+		}
+		for(int k=1;k<=2*i+1;k++)
+		{
+			cout<<"* ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
+*/
