71b41ed126b8a10da1244ab44f27e1dfe98a281f
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-17 22:20:47 +0530

    Update 3.Is Prime.cpp

--- 4.Functions/1.Functions1/3.Is Prime.cpp
+++ 4.Functions/1.Functions1/3.Is Prime.cpp
@@ -3,17 +3,14 @@ using namespace std;
 
 bool isPrime(int x)
 {
-	for(int i=2;i<=x/2;i++)
+	for(int i=2;i<x/2;i++)
 	{
 		if(x%i==0)
 		{
 			return false;
 		}
-		else
-		{
-			return true;
-		}
 	}
+	return true;
 }
 int main()
 {
