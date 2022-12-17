0810d37ec5f578957c2ba7eb49acdb3c234b730c
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-17 22:04:45 +0530

    Create 3.Is Prime.cpp

---
+++ 4.Functions/1.Functions1/3.Is Prime.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int x)
+{
+	for(int i=2;i<=x/2;i++)
+	{
+		if(x%i==0)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+}
+int main()
+{
+	int n;
+	cout<<"Enter Number:";
+	cin>>n;
+	if(isPrime(n))
+	{
+		cout<<"Number is Prime";
+	}
+	else
+	{
+		cout<<"Number is not Prime";
+	}
+	return 0;
+}
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+bool isPrime(int x){
+	int count=0;
+	for(int i=1;i<=x;i++){
+		if((x%i)==0){
+		 count++;	
+		}
+	}
+	if(count==2){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+int main(){
+	int n;
+	cout<<"Enter Number:";
+	cin>>n;
+	if(isPrime(n)){
+		cout<<"Number is Prime";
+	}
+	else{
+		cout<<"Number is Not Prime";
+	}
+	
+}
+*/
