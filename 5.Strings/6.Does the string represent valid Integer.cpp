28fcf94148bd676ac7a0ac3e4e83a3bca5d693f1
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-27 15:20:52 +0530

    Update 6.Does the string represent valid Integer.cpp

--- 5.Strings/6.Does the string represent valid Integer.cpp
+++ 5.Strings/6.Does the string represent valid Integer.cpp
@@ -1,33 +1,112 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool isInteger(string s)
+bool isInteger(string s,int n)
 {
-	string result="";
-	int length=s.length();
-	for(int i=1;i<length;i++)
+	for(int i=n;i<s.length();i++)
+		{
+			if(s[i]>='0'&&s[i]<='9'||s[i]==' ')
+			{
+				continue;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return true;
+}
+void isValid(string str,int a)
+{
+	if(isInteger(str,a))
 	{
-		if(s[i]==' '||(s[i]>='0'&&s[i]<='9')||s[0]=='+'||s[0]=='-')
-		{
-			result+=s[i];
-		}
-		else
-		{
-			return false;
-		}
+		cout<<"The string represents integer";
+	}
+	else
+	{
+		cout<<"The string doesnot represents integer";
 	}
 }
 int main()
 {
-	string integer;
-	cin>>integer;
-	cout<<integer<<"\n";
-	if(isInteger(integer))
+	string str;
+	cout<<"Enter string:";
+	getline(cin,str);
+	if(str.length()>=1)
 	{
-		cout<<"True";
+		if(str[0]=='+'||str[0]=='-'||str[0]==' ')
+		{
+			int a=1;
+			isValid(str,a);
+		}
+		else
+		{
+			int a=0;
+			isValid(str,a);
+		}
 	}
 	else
 	{
-		cout<<"False";
+		cout<<"Enter string of atleast length 1";
 	}
 	return 0;
 }
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+bool isInteger(string s)
+{
+	if(s[0]=='+'||s[0]=='-'||s[0]==' ')
+	{
+		for(int i=1;i<s.length();i++)
+		{
+			if(s[i]>='0'&&s[i]<='9'||s[i]==' ')
+			{
+				continue;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	else
+	{
+		for(int i=0;i<s.length();i++)
+		{
+			if(s[i]>='0'&&s[i]<='9'||s[i]==' ')
+			{
+				continue;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
+int main()
+{
+	string str;
+	cout<<"Enter string:";
+	getline(cin,str);
+	if(str.length()>=1)
+	{
+		if(isInteger(str))
+		{
+			cout<<"The string represents integer";
+		}
+		else
+		{
+			cout<<"The string doesnot represent integer";
+		}
+	}
+	else
+	{
+		cout<<"Enter string of atleast length 1";
+	}
+	return 0;
+}
+*/
