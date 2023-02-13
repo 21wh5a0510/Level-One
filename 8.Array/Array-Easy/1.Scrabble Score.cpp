0097e4c504c27abb16c99dc14d663d027935de86
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2023-02-13 17:23:46 +0530

    Create 1.Scrabble Score.cpp

---
+++ 8.Array/Array-Easy/1.Scrabble Score.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int wordScore(string s)
+{
+ int score=0;
+ int letterscores[]={1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+ for(int i=0;i<s.length();i++)
+ {
+ 	char c=toupper(s[i]);
+    score+=letterscores[c-'A'];
+ }
+ return score;
+}
+int main()
+{
+ string str;
+ cin>>str;
+ cout<<wordScore(str);
+}
