998c5bf290f7f715117ced4f9fd42c654d1bf010
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2023-03-06 14:44:46 +0530

    Update 1.Compute the Hypotenuse.cpp

--- 4.Functions/1.Functions1/1.Compute the Hypotenuse.cpp
+++ 4.Functions/1.Functions1/1.Compute the Hypotenuse.cpp
@@ -1,15 +1,47 @@
+//#include<bits/stdc++.h>
+//using namespace std;
+//
+//float getHypotenuse(float a,float b)
+//{
+//	return sqrt((a*a)+(b*b));
+//}
+//int main()
+//{
+//	float s1,s2;
+//	cout<<"Enter Sides:";
+//	cin>>s1>>s2;
+//	cout<<"The Hypotenuse of the Triangle is "<<getHypotenuse(s1,s2);
+//	return 0;
+//}
 #include<bits/stdc++.h>
 using namespace std;
-
-float getHypotenuse(float a,float b)
-{
-	return sqrt((a*a)+(b*b));
-}
 int main()
 {
-	float s1,s2;
-	cout<<"Enter Sides:";
-	cin>>s1>>s2;
-	cout<<"The Hypotenuse of the Triangle is "<<getHypotenuse(s1,s2);
+	string s;
+	cin>>s;
+//	string rev="";
+//	rev=s;
+//	reverse(s.begin(),s.end());
+//	if(s==rev)
+//	{
+//		cout<<"yes";
+//	}
+//	else
+//	{
+//		cout<<	"No";
+//	}
+	string tmp="";
+    for(int i=s.length()-1;i>0;i--)
+    {
+    	tmp+=s[i];
+	}
+	if(tmp==s)
+	{
+		cout<<"Yes";
+	}
+	else
+	{
+		cout<<"No";
+	}
 	return 0;
 }
