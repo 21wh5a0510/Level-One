74c4610559bfb1481e172fe3adf580c00d45659a
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2023-03-06 14:44:35 +0530

    Update 7.Diamond Pattern.cpp

--- 3.Repetitions/Nested Loops/7.Diamond Pattern.cpp
+++ 3.Repetitions/Nested Loops/7.Diamond Pattern.cpp
@@ -1,3 +1,37 @@
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cout<<"Enter Number:";
+//	cin>>n;
+//	for(int i=0;i<n;i++)
+//	{
+//		for(int j=n-1;j>i;j--)
+//		{
+//			cout<<" ";
+//		}
+//		for(int k=0;k<2*i+1;k++)
+//		{
+//			cout<<"*";
+//		}
+//		cout<<"\n";
+//	}
+//	for(int i=n-1;i>0;i--)
+//	{
+//		for(int j=n;j>i;j--)
+//		{
+//			cout<<" ";
+//		}
+//		for(int k=0;k<2*i-1;k++)
+//		{
+//			cout<<"*";
+//		}
+//		cout<<"\n";
+//	}
+//	return 0;
+//}
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -5,25 +39,25 @@ int main()
 	int n;
 	cout<<"Enter Number:";
 	cin>>n;
-	for(int i=0;i<n;i++)
+	for(int i=1;i<=n;i++)
 	{
-		for(int j=n-1;j>i;j--)
+		for(int j=1;j<=n-i;j++)
 		{
 			cout<<" ";
 		}
-		for(int k=0;k<2*i+1;k++)
+		for(int k=1;k<=2*i-1;k++)
 		{
 			cout<<"*";
 		}
 		cout<<"\n";
 	}
-	for(int i=n-1;i>0;i--)
+	for(int i=1;i<=n-1;i++)
 	{
-		for(int j=n;j>i;j--)
+		for(int j=1;j<=i;j++)
 		{
 			cout<<" ";
 		}
-		for(int k=0;k<2*i-1;k++)
+		for(int k=i;k<=2*n-i-2;k++)
 		{
 			cout<<"*";
 		}
