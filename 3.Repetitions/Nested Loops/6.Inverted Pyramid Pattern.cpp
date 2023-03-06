73cb6ae57c4bd2fd8fbe502564ea9839f5cac598
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2023-03-06 14:44:32 +0530

    Update 6.Inverted Pyramid Pattern.cpp

--- 3.Repetitions/Nested Loops/6.Inverted Pyramid Pattern.cpp
+++ 3.Repetitions/Nested Loops/6.Inverted Pyramid Pattern.cpp
@@ -1,3 +1,26 @@
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cout<<"Enter n:";
+//	cin>>n;
+//	for(int i=n;i>0;i--)
+//	{
+//		for(int k=n;k>i;k--)
+//		{
+//			cout<<"  ";
+//		}
+//		for(int j=1;j<=2*i-1;j++)
+//		{
+//			cout<<"* ";
+//		}
+//		cout<<"\n";
+//	}
+//	return 0;
+//}
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -5,13 +28,13 @@ int main()
 	int n;
 	cout<<"Enter n:";
 	cin>>n;
-	for(int i=n;i>0;i--)
+	for(int i=0;i<n;i++)
 	{
-		for(int k=n;k>i;k--)
+		for(int k=0;k<i;k++)
 		{
 			cout<<"  ";
 		}
-		for(int j=1;j<=2*i-1;j++)
+		for(int j=i;j<2*n-i-1;j++)
 		{
 			cout<<"* ";
 		}
